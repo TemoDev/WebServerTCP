67d90711a989d8e70291b7c30a5fa5bd6658811c
TemoDev <temo.kuliashvili@gmail.com>
2025-05-03 19:11:29 +0400

    Project initialization

---
+++ WebServerTCP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServerTCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ WebServerTCP/WebServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServerTCP
+{
+    internal class WebServer
+    {
+    }
+}
